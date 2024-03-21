e23408159ef2485ed40dadc84cc870f47b134a48
Daniel Young <Daniel.w.young.jr@gmail.com>
2024-03-21 18:46:47 -0400

    Added m_ShaderPrograms field

--- src/rendering/renderer.h
+++ src/rendering/renderer.h
@@ -15,10 +15,19 @@ public:
 
 	void Render();
 
+	template<typename... Args>
+	std::shared_ptr<ShaderProgram> AddShaderProgram(Args... args)
+	{
+		std::shared_ptr shaderProgram = std::make_shared<ShaderProgram>(args...);
+		m_ShaderPrograms.push_back(shaderProgram);
+		return shaderProgram;
+	}
+
 	void RegisterVAOShaderMatch(GLuint id, GLuint sp) { m_VAOtoShaderProgram.insert({ id, sp }); }
 
 private:
 	std::shared_ptr<std::vector<std::shared_ptr<VAO>>> m_VAOs = nullptr;
+	std::vector<std::shared_ptr<ShaderProgram>> m_ShaderPrograms = std::vector<std::shared_ptr<ShaderProgram>>();
 	std::unordered_map<GLuint, GLuint> m_VAOtoShaderProgram = {};
 };
 
