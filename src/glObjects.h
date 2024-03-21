3b363357017fd6c6fdbea6c997a9f52dc6263ebf
Daniel Young <Daniel.w.young.jr@gmail.com>
2024-03-21 18:04:59 -0400

    Fixed error that was causing nothing to show up. glBufferData size argument was wrong

--- src/glObjects.h
+++ src/glObjects.h
@@ -41,7 +41,7 @@ public:
 	void SetBufferData(const std::vector<Vertex>& vertices) const
 	{
 		Bind();
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
 
 		SetVertexAttributesPointers();
 	}
@@ -63,7 +63,7 @@ public:
 	void SetBufferData(const std::vector<GLuint>& indices) const
 	{
 		Bind();
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
 	}
 
 private:
