27e39e5b16f32e4201ba4671eaf8e4ebe0016722
Daniel Young <Daniel.w.young.jr@gmail.com>
2024-03-26 21:26:47 -0400

    Removed physx usage

--- src/physics/simulator.h
+++ src/physics/simulator.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <physx/PxPhysicsAPI.h>
-
 #include <vector>
 
 class Simulator
@@ -10,22 +8,11 @@ public:
 	Simulator() { Init(); }
 	~Simulator() { CleanUp(); }
 
-	void Step();
-
-	void AddDroplet(physx::PxVec3 center, physx::PxReal radius);
-
-	static physx::PxRigidDynamic* CreateDynamic(const physx::PxTransform& t, const physx::PxGeometry& geometry, const physx::PxMaterial& material, const physx::PxVec3& velocity = physx::PxVec3(0));
-	static physx::PxRigidStatic* CreateStatic(const physx::PxTransform& t, const physx::PxGeometry& geometry, const physx::PxMaterial& material);
-
-	static physx::PxMaterial* CreateMaterial(physx::PxReal staticFriction, physx::PxReal dynamicFriction, physx::PxReal restitution);
-	static physx::PxTriangleMesh* CreateTriangleMesh(const std::vector<physx::PxVec3>& vertices, const std::vector<physx::PxU32>& indices);
-
-	void ClearDroplets();
+	void Step() {}
 public:
-	std::vector<physx::PxRigidDynamic*> m_Droplets = std::vector<physx::PxRigidDynamic*>();
 
 private:
-	void Init();
+	void Init() {}
 
-	void CleanUp();
+	void CleanUp() {}
 };
