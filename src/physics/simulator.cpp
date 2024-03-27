74d674b657498a849ea9d8968c8122369d9c1126
Daniel Young <Daniel.w.young.jr@gmail.com>
2024-03-26 23:41:06 -0400

    Added collision handling between particles and terrain

--- src/physics/simulator.cpp
+++ src/physics/simulator.cpp
@@ -1,1 +1,29 @@
 #include "simulator.h"
+
+void Simulator::HandleCollisions()
+{
+	if (!m_TerrainGeometry) return;
+
+	const float maxX = static_cast<float>(m_TerrainGeometry->GetWidth() / 2U);
+	const float minX = -maxX;
+	const float maxZ = static_cast<float>(m_TerrainGeometry->GetLength() / 2U);
+	const float minZ = -maxZ;
+
+	for (auto& particle : m_Particles)
+	{
+		glm::vec3 center = particle.GetCenter();
+		// Check that center is within height field bounds
+		if (center.x >= minX && center.x <= maxX && center.z >= minZ && center.z <= maxZ)
+		{
+			float offset = abs(m_TerrainGeometry->GetHeight(center.x, center.z) - center.y);
+			// Check that sphere is intersecting height field
+			if (offset < particle.GetRadius())
+				particle.SetCenter(center + m_TerrainGeometry->GetNormal(center.x, center.z) * offset); // Push sphere outwards
+		}
+	}
+}
+
+void Simulator::ApplySPH()
+{
+
+}
--- src/physics/simulator.h
+++ src/physics/simulator.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <memory>
 #include <vector>
+
+#include "primitives/sphere.h"
+#include "primitives/heightMap.h"
 
 class Simulator
 {
@@ -8,11 +12,21 @@ public:
 	Simulator() { Init(); }
 	~Simulator() { CleanUp(); }
 
-	void Step() {}
-public:
+	void SetDeltaTime(float deltaTime) { m_DeltaTime = deltaTime; }
+	float GetDeltaTime() const { return m_DeltaTime; }
+
+	void Step();
 
 private:
 	void Init() {}
 
+	void HandleCollisions();
+	void ApplySPH();
+
 	void CleanUp() {}
+
+private:
+	std::unique_ptr<Heightfield> m_TerrainGeometry = nullptr;
+	std::vector<DynamicSphere> m_Particles = std::vector<DynamicSphere>();
+	float m_DeltaTime = 0.0f;
 };
